50896a57b1d900078549e4902afbc3aa41394f2e
chenyii426 <chenyii426@gmail.com>
2020-08-17 21:45:03 -0700

    initial commit

---
+++ discovery-service/src/test/java/com/cy426/discovery/service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy426.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
